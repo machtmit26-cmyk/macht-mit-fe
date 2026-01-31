5e3821cfb827949b2cd3c2849c2d25c7d2702cf8
AbhishekisBack <26abhishek.y@gmail.com>
2026-01-31 11:14:20 +0530

    login route fix

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import Footer from "../common/footer/Footer";
 import axios from "axios";
+
 import {
   Box,
   Button,
@@ -15,11 +16,13 @@ import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import { setAuthCookie } from "../auth";
 import AppToast from "../toast/AppToast";
+import { useHistory } from "react-router-dom";
 
 const PRIMARY_COLOR = "#20b2a6";
 const PAGE_BG = "#f2f2f2";
 
 const LoginPage1 = () => {
+  const history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
